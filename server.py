c348f31091251314f4c5bff98a53cdb65b772058
akram-anissi <148265887+akram-anissi@users.noreply.github.com>
2025-01-19 02:38:24 +0100

    init

--- server.py
+++ server.py
@@ -3,8 +3,20 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 import cv2
 import base64
 import numpy as np
-import zlib
 import random
+
+def compress_frame(frame):
+    height, width = frame.shape[:2]
+    max_width = 640
+    if width > max_width:
+        ratio = max_width / width
+        new_width = int(width * ratio)
+        new_height = int(height * ratio)
+        frame = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
+    _, buffer = cv2.imencode('.jpg', frame, encode_param)
+    return buffer
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -20,10 +32,9 @@ def index():
 def handle_create_room(data):
     room_id = str(random.randint(1000, 9999))
     name = data['name']
-    rooms[room_id] = {'participants': {request.sid: name}}
+    rooms[room_id] = {'host': request.sid, 'participants': {request.sid: name}}
     join_room(room_id)
     emit('room_created', {'room_id': room_id})
-    emit('joined_room', {'room_id': room_id, 'name': name})
 
 @socketio.on('join_room')
 def handle_join_room(data):
@@ -37,31 +48,11 @@ def handle_join_room(data):
     emit('joined_room', {'room_id': room_id, 'name': name})
     emit('participant_joined', {'name': name}, to=room_id)
 
-@socketio.on('leave_room')
-def handle_leave_room(data):
-    room_id = data['room_id']
-    leave_room(room_id)
-    if room_id in rooms:
-        name = rooms[room_id]['participants'].pop(request.sid, None)
-        emit('participant_left', {'name': name}, to=room_id)
-
 @socketio.on('video_frame')
 def handle_video_frame(data):
     room_id = data['room_id']
-    frame_data = zlib.decompress(base64.b64decode(data['frame']))
-    emit('video_frame', {'frame': frame_data}, to=room_id, include_self=False)
-
-@socketio.on('audio_frame')
-def handle_audio_frame(data):
-    room_id = data['room_id']
-    emit('audio_frame', data, to=room_id, include_self=False)
-
-@socketio.on('chat_message')
-def handle_chat_message(data):
-    room_id = data['room_id']
-    name = rooms[room_id]['participants'].get(request.sid, 'Unknown')
-    message = data['message']
-    emit('chat_message', {'name': name, 'message': message}, to=room_id)
+    if request.sid == rooms[room_id]['host']:
+        emit('video_frame', {'frame': data['frame']}, to=room_id, include_self=False)
 
 @socketio.on('disconnect')
 def handle_disconnect():
